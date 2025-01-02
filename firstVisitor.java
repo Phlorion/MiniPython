01b38df33b205d45b1d5450d6f319dc0d737f12c
Gaspar <rigos.gasparis@gmail.com>
2025-01-02 21:34:53 +0200

    error 1 completed (i think)

--- firstVisitor.java
+++ firstVisitor.java
@@ -4,12 +4,16 @@ import java.util.*;
 
 public class firstVisitor extends DepthFirstAdapter  {
     private Hashtable symtable;	
+    private Hashtable valuetable;
 
-	firstVisitor(Hashtable symtable) 
+	firstVisitor(Hashtable symtable, Hashtable valuetable) 
 	{
 		this.symtable = symtable;
+        this.valuetable = valuetable;
 	}
 
+
+    // assign statements
 	public void inAAssignEqStatement(AAssignEqStatement node) 
 	{
 		String varName = node.getId().toString();
@@ -19,12 +23,55 @@ public class firstVisitor extends DepthFirstAdapter  {
         // System.out.println("Oristike sti grammh " + line + " h metablhth " + varName);
 	}
 
+    public void inAAssignMineqStatement (AAssignMineqStatement node){
+        String varName = node.getId().toString();
+		int line = node.getId().getLine();
+        int pos = node.getId().getPos();
+        if(!symtable.containsKey(varName)){
+            System.out.println("Line: " + line + " Column: " + pos + ", variable " + varName.trim() + " not defined");
+        }
+    }
+
+    public void inAAssignDiveqStatement (AAssignDiveqStatement node){
+        String varName = node.getId().toString();
+		int line = node.getId().getLine();
+        int pos = node.getId().getPos();
+        if(!symtable.containsKey(varName)){
+            System.out.println("Line: " + line + " Column: " + pos + ", variable " + varName.trim() + " not defined");
+        }
+    }
+
+    public void inAArrayAssignStatement(AArrayAssignStatement node){
+        TId id = node.getId();
+        if(!symtable.containsKey(id.toString())){
+            System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+        }
+
+        PExpression index = node.getIndex();
+        if(index instanceof AIdExpression){
+            TId indexId = ((AIdExpression)index).getId();
+            if(!symtable.containsKey(indexId.toString())){
+                System.out.println("Line: " + indexId.getLine() + " Column: " + indexId.getPos() + ", variable " + indexId.toString().trim() + " not defined");
+            }
+        }
+
+        PExpression rightSide = node.getRightSide();
+        if(rightSide instanceof AIdExpression){
+            TId rightsideId = ((AIdExpression)rightSide).getId();
+            if(!symtable.containsKey(rightsideId.toString())){
+                System.out.println("Line: " + rightsideId.getLine() + " Column: " + rightsideId.getPos() + ", variable " + rightsideId.toString().trim() + " not defined");
+            }
+        }
+    }
+
+
+    // print statement
     public void inAPrintStatement(APrintStatement node){
         PExpression firstExp = node.getL();
         if(firstExp instanceof AIdExpression){
             TId firstId = ((AIdExpression)firstExp).getId();
             if(!symtable.containsKey(firstId.toString())){
-                System.out.println("Line: " + firstId.getLine() + ", " + firstId.toString().trim() + " not defined");
+                System.out.println("Line: " + firstId.getLine() + " Column: " + firstId.getPos() + ", variable " + firstId.toString().trim() + " not defined");
             }
         }
 
@@ -33,11 +80,309 @@ public class firstVisitor extends DepthFirstAdapter  {
             if(pexp instanceof AIdExpression){
                 TId id = ((AIdExpression)pexp).getId();
                 if(!symtable.containsKey(id.toString())){
-                    System.out.println("Line: " + id.getLine() + ", " + id.toString().trim() + " not defined");
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
                 }
             }
         }
+    }
+
+    // return statement
+    public void inAReturnStatement(AReturnStatement node){
+        PExpression exp = node.getExpression();
+        if(exp instanceof AIdExpression){
+            TId firstId = ((AIdExpression)exp).getId();
+            if(!symtable.containsKey(firstId.toString())){
+                System.out.println("Line: " + firstId.getLine() + " Column: " + firstId.getPos() + ", variable " + firstId.toString().trim() + " not defined");
+            }
+        }
+    }
+
+    // for statement
+    public void inAForStatement(AForStatement node){
+        TId element = node.getElement();
+        if(!symtable.containsKey(element.toString())){
+            System.out.println("Line: " + element.getLine() + " Column: " + element.getPos() + ", variable " + element.toString().trim() + " not defined");
+        }
+
+        TId collection = node.getCollection();
+        if(!symtable.containsKey(collection.toString())){
+            System.out.println("Line: " + collection.getLine() + " Column: " + collection.getPos() + ", variable " + collection.toString().trim() + " not defined");
+        }
+
+    }
+
+    // assert statement
+    public void inAAssertStatement (AAssertStatement node){
+        PExpression l_exp = node.getL();
+        if(l_exp instanceof AIdExpression){
+            TId exp_id = ((AIdExpression)l_exp).getId();
+                if(!symtable.containsKey(exp_id.toString())){
+                    System.out.println("Line: " + exp_id.getLine() + " Column: " + exp_id.getPos() + ", variable " + exp_id.toString().trim() + " not defined");
+                }
+        }
+
+        for (Object exp : node.getCommaExp()) {
+            if((PExpression)exp instanceof AIdExpression){
+                TId cexp_id = ((AIdExpression)exp).getId();
+                if(!symtable.containsKey(cexp_id.toString())){
+                    System.out.println("Line: " + cexp_id.getLine() + " Column: " + cexp_id.getPos() + ", variable " + cexp_id.toString().trim() + " not defined");
+                }
+            }
+        }
+    }
+
+    // gia kapoio logo den douleuei
+    //
+    // // id expression 
+    // public void AIdExpression(AIdExpression node){
+    //     TId id = node.getId();
+    //     if(!symtable.containsKey(id)){
+    //         System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+    //     }
+    // }
+
+    // arithmetic expressions
+    public void inAAdditionExpression (AAdditionExpression node){
+        PExpression left = node.getL();
+        PExpression right = node.getR();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    public void inASubtractionExpression (ASubtractionExpression node){
+        PExpression left = node.getL();
+        PExpression right = node.getR();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    public void inAMultiplicationExpression (AMultiplicationExpression node){
+        PExpression left = node.getL();
+        PExpression right = node.getR();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    public void inADivisionExpression (ADivisionExpression node){
+        PExpression left = node.getL();
+        PExpression right = node.getR();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
 
     
+    // list element expression
+    public void inAListElementExpression (AListElementExpression node){
+        TId id = node.getId();
+        if(!symtable.containsKey(id.toString())){
+            System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+        }
+
+        PExpression exp = node.getExpression();
+        if(exp instanceof AIdExpression){
+            TId exp_id = ((AIdExpression)exp).getId();
+                if(!symtable.containsKey(exp_id.toString())){
+                    System.out.println("Line: " + exp_id.getLine() + " Column: " + exp_id.getPos() + ", variable " + exp_id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    // len expression
+    public void inALengthExpression (ALengthExpression node){
+        PExpression exp = node.getExpression();
+        if(exp instanceof AIdExpression){
+            TId exp_id = ((AIdExpression)exp).getId();
+                if(!symtable.containsKey(exp_id.toString())){
+                    System.out.println("Line: " + exp_id.getLine() + " Column: " + exp_id.getPos() + ", variable " + exp_id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    // parenthesis expression
+    public void inAParenthesisExpressionExpression (AParenthesisExpressionExpression node){
+        PExpression exp = node.getExpression();
+        if(exp instanceof AIdExpression){
+            TId exp_id = ((AIdExpression)exp).getId();
+                if(!symtable.containsKey(exp_id.toString())){
+                    System.out.println("Line: " + exp_id.getLine() + " Column: " + exp_id.getPos() + ", variable " + exp_id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+
+    // min expression
+    public void inAMinExpression (AMinExpression node){
+    }
+
+    // max expression
+    public void inAMaxExpression (AMaxExpression node){
+    }
+
+    // list expression
+    public void inAListExpression (AListExpression node){
+    }
+
+
+    // ascii expression
+    public void inAAsciiValExpression (AAsciiValExpression node){
+        TId id = node.getId();
+        if(!symtable.containsKey(id.toString())){
+            System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+        }
+    }
+
+
+    // comparisons
+    public void inAGreatComparisons (AGreatComparisons node){
+        PExpression left = node.getLexpr();
+        PExpression right = node.getRexpr();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    public void inALessComparisons (ALessComparisons node){
+        PExpression left = node.getLexpr();
+        PExpression right = node.getRexpr();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    
+    public void inAGreatEqComparisons (AGreatEqComparisons node){
+        PExpression left = node.getLexpr();
+        PExpression right = node.getRexpr();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    public void inALessEqComparisons (ALessEqComparisons node){
+        PExpression left = node.getLexpr();
+        PExpression right = node.getRexpr();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+    public void inANotEqComparisons (ANotEqComparisons node){
+        PExpression left = node.getLexpr();
+        PExpression right = node.getRexpr();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+    
+    public void inACompEqComparisons (ACompEqComparisons node){
+        PExpression left = node.getLexpr();
+        PExpression right = node.getRexpr();
+        if(left instanceof AIdExpression){
+            TId id = ((AIdExpression)left).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+        if(right instanceof AIdExpression){
+            TId id = ((AIdExpression)right).getId();
+                if(!symtable.containsKey(id.toString())){
+                    System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+                }
+        }
+    }
+
+
+
+    // id.func_call value
+    public void inAFuncCallValue (AFuncCallValue node){
+            TId id = node.getId();
+            if(!symtable.containsKey(id.toString())){
+                System.out.println("Line: " + id.getLine() + " Column: " + id.getPos() + ", variable " + id.toString().trim() + " not defined");
+            }
     }
 }
